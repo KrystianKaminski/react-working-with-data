6d34b37c2c3f350876490f20b56bf0e8407d9cd0
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-22 16:42:59 +0100

    Taking input from the user

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import Form from './Form';
 
-let data = [
-  {
-    name: "Philippe Bruhat (BooK)",
-    avatar_url: "https://avatars0.githubusercontent.com/u/44421?v=4",
-    company: "Booking.com"
-  },
-  {
-    name: "Inzaghi",
-    avatar_url: "https://avatars2.githubusercontent.com/u/7274953?v=4",
-    company: "-"
-  },
-  {
-    name: "pato",
-    avatar_url: "https://avatars0.githubusercontent.com/u/608083?v=4",
-    company: "-"
-  }
-]
+
 
 class App extends Component {
+  state = {
+    cards: [
+      {
+        name: "Philippe Bruhat (BooK)",
+        avatar_url: "https://avatars0.githubusercontent.com/u/44421?v=4",
+        company: "Booking.com"
+      },
+      {
+        name: "Inzaghi",
+        avatar_url: "https://avatars2.githubusercontent.com/u/7274953?v=4",
+        company: "-"
+      },
+      {
+        name: "pato",
+        avatar_url: "https://avatars0.githubusercontent.com/u/608083?v=4",
+        company: "-"
+      }
+    ]
+  }
   render() {
     return (
       <div>
         <Form />
         <CardList
-          cards={data}
+          cards={this.state.cards}
         />
       </div>
     );
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 class Form extends React.Component {
+
+    state = {
+        userName: ''
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+    }
     render() {
         return (
-            <form>
+            <form
+                onSubmit={this.handleSubmit}
+            >
                 <input
                     type="text"
+                    value={this.state.userName}
+                    onChange={(e) => this.setState({ userName: e.target.value })}
                     placeholder="Github username"
                 />
                 <button
