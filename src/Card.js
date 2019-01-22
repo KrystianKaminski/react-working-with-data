be9e18b783293654c9263c45a724b127d8b5e6cd
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-22 16:18:25 +0100

    CSS in React

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 const Card = (props) => {
     return (
-        <div>
+        <div style={{
+            margin: '1em'
+        }}>
             <img src="http://placehold.it/75" />
-            <div>
-                Name here...
+            <div style={{
+                display: 'inline-block',
+                marginLeft: 10
+            }}>
+                <div style={{
+                    fontSize: '1.25em',
+                    fontWeight: 'bold'
+                }}>
+                    Name here...
+                </div>
+
+                <div>
+                    Company name here...
             </div>
-            <div>
-                Company name here...
             </div>
         </div>
     )
