a64b2a3970bcd7c1e599567b7e1a3436e30f8a54
KrystianKaminski <krystiankaminski07@gmail.com>
2019-01-22 16:14:13 +0100

    Card component created

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = (props) => {
+    return (
+        <div>
+            <img src="http://placehold.it/75" />
+            <div>
+                Name here...
+            </div>
+            <div>
+                Company name here...
+            </div>
+        </div>
+    )
+}
+
+export default Card
